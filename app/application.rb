9a3b333b6cbfac2f97c021df534dee5a6bdc5352
github username <github email address>
2019-10-05 14:04:59 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
         item_name = req.path.split("/items/").last
   
         item = @@items.find do |i|
-            i.name == item_name
+            i.name.match(item_name)
         end
   
         if item == nil
